1cfea0e9433b58de34fee646dd65f00e3b831385
uddievastav2006 <uddievastav2006@hotmail.com>
2015-05-14 09:54:00 -0400

    Introduced sample logic to change the color for the transactions

--- Calculator/CheckBook/Transaction.cs
+++ Calculator/CheckBook/Transaction.cs
@@ -6,6 +6,8 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.Windows.Media;
+using System.Windows;
 
 namespace Calculator.CheckBook
 {
@@ -49,8 +51,37 @@ namespace Calculator.CheckBook
         private double _Amount;
         public double Amount
         {
-            get { return _Amount; }
-            set { _Amount = value; OnPropertyChanged(); OnPropertyChanged("Currency2"); }
+            get
+            {
+                if (_Amount > 10)
+                {
+                    SolidColorBrush MyBrush = Brushes.Red;
+                    Application.Current.Resources["DynamicBG"] = MyBrush;
+                }
+                else
+                {
+                    SolidColorBrush MyBrush = Brushes.Green;
+                    Application.Current.Resources["DynamicBG"] = MyBrush;
+                }
+
+                return _Amount;
+            }
+            set
+            {
+                if (value > 10)
+                {
+                    SolidColorBrush MyBrush = Brushes.Red;
+                    Application.Current.Resources["DynamicBG"] = MyBrush;
+                }
+                else
+                {
+                    SolidColorBrush MyBrush = Brushes.Green;
+                    Application.Current.Resources["DynamicBG"] = MyBrush;
+                }
+                _Amount = value;
+
+                OnPropertyChanged("Amount"); OnPropertyChanged("Currency2");
+            }
         }
 
         public double Currency2
